e7f2a3fe868f383cbe883d32c3dda5c849dc3283
Kwizera Emmanuel <kwizeraemmanuel11@gmail.com>
2023-04-25 08:02:31 +0530

    initial commit

---
+++ src/main/java/com/project/onlinecourierservicesystem/OnlineCourierServiceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.onlinecourierservicesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourierServiceSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCourierServiceSystemApplication.class, args);
+    }
+
+}
